0ed012a07b73e3c3fa5145c8eec0cbb57c933c57
Jozsef Kiss <kissjozsefandras@gmail.com>
2025-07-19 21:11:22 +0200

    Refactored project to use logging, fixed minor issues with training pipeline and model saving logic

--- src/main.py
+++ src/main.py
@@ -1,3 +1,8 @@
+# Sources used:
+# https://medium.com/@yennhi95zz/logging-the-effective-management-of-machine-learning-systems-e1eb04e74eb5
+
+
+import logging
 from datasets import CBISDDSMDataset, MIASDataset
 from preprocessing import apply_transforms
 from train_classification_layer import prepare_model, train_model
@@ -11,21 +16,34 @@ import json
 
 
 def main():
+
     config = {
         "batch_size": 32,
-        "epochs": 5,
-        "learning_rate": 0.001,
+        "epochs": 3,
         "num_classes": 2,
         "device": torch.device("cuda" if torch.cuda.is_available() else "cpu"),
         "checkpoints_path": "./checkpoints",
         "results_path": "./results",
+        "logs_path": "./logs"
     }
+
+    # Setting up logging
+    workflow_start_timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    log_path = os.path.join(config["logs_path"], f"training_log_{workflow_start_timestamp}.log")
+    logging.basicConfig(level=logging.DEBUG,
+                        format="%(asctime)s - %(levelname)s - %(message)s",
+                        handlers = [logging.FileHandler(log_path), logging.StreamHandler()])
+    
+    logging.info(f"Logging initialised, log file: {log_path}")
 
     # Model training main parameters
     Models = ["resnet50", "densenet121", "inception_v3"]
-    LR = [1e-4, 1e-3]
-    Optimizers = ["adam", "adamw", "sgd"]
-    Depth = ["classifier_only", "last_layer", "last_2_layers"]
+    #LR = [1e-4, 1e-3]
+    LR = [1e-3]
+    #Optimizers = ["adam", "adamw", "sgd"]
+    Optimizers = ["adam"]
+    #Depth = ["classifier_only", "last_layer", "last_2_layers"]
+    Depth = ["classifier_only", "last_layer"]
 
     best_models = {
         "resnet50": {"learning_rate:": 0.0, "optimizer": None, "depth": None, "accuracy": 0.0, "f1_score": 0.0, "AUC": 0.0},
@@ -38,87 +56,127 @@ def main():
     os.makedirs(config["results_path"], exist_ok=True)
 
     # Load datasets
-    print("Loading datasets...")
-    train_mass_df = pd.read_csv("./data/processed/mass_case_description_train_set_mapped.csv")
-    val_mass_df = pd.read_csv("./data/processed/mass_case_description_test_set_mapped.csv")
+    logging.info("Loading datasets...")
+    try:
+        train_mass_df = pd.read_csv("./data/processed/mass_case_description_train_set_mapped.csv")
+        val_mass_df = pd.read_csv("./data/processed/mass_case_description_test_set_mapped.csv")
+        logging.info("Datasets successfully loaded.")
+    except Exception as e:
+        logging.error(f"Dataset loading failed with error {str(e)}")
+        return
 
     overall_results = []
-
     total_combinations = len(Models) * len(LR) * len(Optimizers) * len(Depth)
+    logging.info(f"Training will be initialised for {total_combinations} configurations.")
     combination_count = 0
+
+    training_start = datetime.now()
+    logging.info(f"Systematic model training startet at {training_start.strftime("%Y-%m-%d %H:%M:%S")}")
 
     # Systematic model training
     for model_name in Models:
-        print(f"\n Model training workflow initiated for {model_name}...")
+        model_training_start = datetime.now()
+        logging.info(f"Model training workflow initiated for {model_name} family... at {model_training_start.strftime("%Y-%m-%d %H:%M:%S")}")
+        
         train_transform, val_transform = apply_transforms(model_name)
 
         train_mass_dataset = CBISDDSMDataset(train_mass_df, transform=train_transform)
         val_mass_dataset = CBISDDSMDataset(val_mass_df, transform=val_transform)
-        print(f"\n Datasets loaded successfully...")
+        logging.debug("Training and validation datasets passed to the model.")
 
         train_loader = DataLoader(train_mass_dataset, 
                                 batch_size=config["batch_size"],
                                 shuffle=True,
                                 num_workers=4,
                                 pin_memory=True)
+        logging.debug("Training loader initialised.")
         
         val_loader = DataLoader(val_mass_dataset, 
                                 batch_size=config["batch_size"],
                                 shuffle=False,
                                 num_workers=4,
                                 pin_memory=True)
+        logging.debug("Validation loader initialised.")
+
+        # store best performing model constellation
+        model_best_f1 = 0.0
+        model_best_result = None
+        model_best_states = None
         
         for lr, optimizer, depth in product(LR, Optimizers, Depth):
             combination_count += 1
-            print(f"\n Training progress {combination_count}/{total_combinations}")
-            print(f"\n Training parameters: LR:{lr} Optimizer:{optimizer} Depth:{depth}")
-
-            overall_best_f1 = 0.0
+            logging.info(f"Training progress {combination_count}/{total_combinations}")
+            logging.info(f"Training parameters: LR:{lr} Optimizer:{optimizer} Depth:{depth}")
 
             try:
                 model = prepare_model(model_name, config["num_classes"], depth)
-                result, best_model_state_dict, best_optimizer_state_dict, best_epoch, best_f1 = train_model(model_name,
-                                    lr, optimizer, depth, 
-                                    model, 
-                                    config["device"], 
-                                    train_loader, 
-                                    val_loader, 
-                                    config)
-                print(f"\n Training with parameter combination LR: {lr}; Optimizer:{optimizer}; Depth:{depth}")
+                logging.debug(f"Model initialised - {model_name}.")
+                logging.info("Training workflow started...")
+                result, best_model_state_dict, best_optimizer_state_dict, best_epoch, best_f1 = train_model(model_name=model_name,
+                                    learning_rate=lr, optimizer_name=optimizer, training_depth=depth, 
+                                    model=model, 
+                                    device=config["device"], 
+                                    train_dataloader=train_loader, 
+                                    validation_dataloader=val_loader, 
+                                    config=config)
+                logging.info(f"{model_name} training for parameter combination LR: {lr}; Optimizer:{optimizer}; Depth:{depth} finished with F1-score {best_f1}.")
                 overall_results.append(result)
 
-                if best_f1 > overall_best_f1:
+                if best_f1 > model_best_f1:
                     output_path = os.path.join(config["checkpoints_path"], f"{model_name}_best.pth")
-                    overall_best_f1 = best_f1
-                    torch.save({
-                        "epoch": best_epoch,
+                    model_best_f1 = best_f1
+                    model_best_result = result
+                    model_best_states = {
                         "model_state_dict": best_model_state_dict,
                         "optimizer_state_dict": best_optimizer_state_dict,
-                        "val_f1": best_f1,
-                        "config": config
-                    },output_path)
-
-                #save best performing model
-                
+                        "epoch": best_epoch,
+                        "f1_score": best_f1
+                    }
+                    logging.info(f"New best model for {model_name}: F1={best_f1:.2f}")
+            
             except Exception as e:
-                print(f"Unexpected error raised: {str(e)}")
+                logging.error(f"Training failed for {model_name} with params LR:{lr}, Optimizer:{optimizer}, Depth:{depth}. Error: {str(e)}")
                 continue
-        
-        # Store results
-        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-        results_file = os.path.join(config["results_path"], f"training_results_{timestamp}.json")
-        with open(results_file, 'w') as f:
-            json.dump(overall_results, f, indent=2)
 
-        # Training summary
-        overall_results.sort(key=lambda x: x["best_val_f1"], reverse=True) # sort by validation accuracy
+        if model_best_states is not None:
+            torch.save({
+                "epoch": model_best_states["epoch"],
+                "model_state_dict": model_best_states["model_state_dict"],
+                "optimizer_state_dict": model_best_states["optimizer_state_dict"],
+                "val_f1": model_best_states["f1_score"],
+                "config": config
+                }, output_path)
+            logging.info(f"Best model version for {model_name} saved.")
 
-        print(f"\n Model training completed - Top 5 performer models:")
-        for i, result in enumerate(overall_results[:5], 1):
-            print(f"\n{i}. {result['model_name']} (LR={result['learning_rate']}, Optimizer={result['optimizer']}, Depth={result['training_depth']})")
-            print(f"   Val Acc: {result['best_val_acc']:.4f}")
-            print(f"   Val F1:  {result['best_val_f1']:.4f}")
-            print(f"   Val AUC: {result['best_val_roc_auc']:.4f}")
+            best_models[model_name].update({
+                "learning_rate": model_best_result["LR"],
+                "optimizer": model_best_result["optimizer"],
+                "depth": model_best_result["training_depth"],
+                "train_loss": model_best_result["train_loss"],
+                "train_val": model_best_result["train_val"],
+                "val_acc": model_best_result["val_acc"],
+                "val_loss": model_best_result["val_loss"],
+                "val_recall": model_best_result["val_recall"],
+                "val_precision": model_best_result["val_precision"],
+                "f1_score": model_best_result["val_f1"],
+                "AUC": model_best_result["val_roc_auc"],
+                "val_specificity": model_best_result["val_specificity"],
+                "epoch": model_best_states["epoch"]
+            })
+
+            logging.info(f"Best transfer learning parameters: LR={model_best_result['learning_rate']}, Optimizer={model_best_result['optimizer']}, Depth={model_best_result['training_depth']}.")
+        else:
+            logging.error(f"{model_name} training failed to save best performing model data.")
+           
+    # Store all training results
+    results_file = os.path.join(config["results_path"], f"training_results_{model_name}.json")
+    with open(results_file, 'w') as f:
+        json.dump(overall_results, f, indent=2)
+    
+    # Best models summary
+    best_models_file = os.path.join(config["results_path"], f"training_results_best_models.json")
+    with open(best_models_file, 'w') as f:
+        json.dump(best_models, f, indent=2)
 
 if __name__ == "__main__":
     import multiprocessing
--- src/train_classification_layer.py
+++ src/train_classification_layer.py
@@ -18,19 +18,10 @@ import torch.nn as nn
 import torchvision.models as models
 import os
 import pandas as pd
+import logging
 
 # Model evaluation metrics
 from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score,roc_auc_score, confusion_matrix
-
-""" # Configuration
-config = {
-    "batch_size": 32,
-    "epochs": 5,
-    "learning_rate": 0.001,
-    "num_classes": 2,
-    "device": torch.device("cuda" if torch.cuda.is_available() else "cpu"),
-    "checkpoints_path": "./checkpoints",
-} """
 
 # code adapted from https://www.geeksforgeeks.org/deep-learning/how-to-handle-overfitting-in-pytorch-models-using-early-stopping/
 class Earlystopping:
@@ -64,6 +55,7 @@ def train_epoch(model, dataloader, criterion, optimizer, device):
     total_predictions = 0
 
     num_batches = len(dataloader)
+    logging.debug(f"Starting model training epoch with {num_batches} batches...")
 
     # Iterate through all the batches in the dataset
     for batch_idx, (inputs, labels) in enumerate(dataloader):
@@ -86,6 +78,7 @@ def train_epoch(model, dataloader, criterion, optimizer, device):
     
     epoch_loss = running_loss / num_batches
     epoch_acc = correct_predictions / total_predictions
+    logging.debug(f"Model training epoch completed. Loss={epoch_loss:.4f}, Accuracy={epoch_acc:.4f}")
 
     return epoch_acc, epoch_loss
     
@@ -98,8 +91,8 @@ def validate_epoch(model, dataloader, criterion, device):
     all_preds = []
     all_probs = []
 
-
     num_batches = len(dataloader)
+    logging.debug(f"Starting model validation epoch with {num_batches} batches...")
 
     # Run validation without gradient calculation
     with torch.no_grad():
@@ -131,6 +124,17 @@ def validate_epoch(model, dataloader, criterion, device):
     tn, fp, fn, tp = confusion_matrix(all_labels, all_preds).ravel()
     epoch_specificity = tn / (tn+fp) if (tn+fp) != 0 else float("nan")
 
+    logging.debug(
+        f"Validation epoch completed: "
+        f"Loss={epoch_loss:.4f}, "
+        f"Accuracy={epoch_accuracy:.4f}, "
+        f"Precision={epoch_precision:.4f}, "
+        f"Recall={epoch_recall:.4f}, "
+        f"F1={epoch_f1:.4f}, "
+        f"Specificity={epoch_specificity:.4f}, "
+        f"ROC-AUC={epoch_roc_auc:.4f}"
+    )
+
     return epoch_accuracy, epoch_loss, epoch_recall, epoch_precision, epoch_f1, epoch_roc_auc, epoch_specificity
 
 def prepare_model(model_name: str, num_classes: int, training_depth: str) -> torch.nn.Module:
@@ -146,46 +150,58 @@ def prepare_model(model_name: str, num_classes: int, training_depth: str) -> tor
         torch.nn.Module: Prepared model
     """
 
+    logging.debug(f"Preparing {model_name} with {num_classes} classes and {training_depth} depth.")
+
     if model_name == "resnet50":
         model = models.resnet50(weights=models.ResNet50_Weights.IMAGENET1K_V2)
         classifier_name = "fc"
+        logging.debug("ResNet50 model loaded with ImageNet weights")
     elif model_name == "densenet121":
         model = models.densenet121(weights=models.DenseNet121_Weights.IMAGENET1K_V1)
         classifier_name = "classifier"
+        logging.debug("DenseNet121 model loaded with ImageNet weights")
     elif model_name == "inception_v3":
         model = models.inception_v3(weights=models.Inception_V3_Weights.IMAGENET1K_V1, aux_logits=True)
         model.aux_logits = False
         classifier_name = "fc"
+        logging.debug("Inception_V3 model loaded with ImageNet weights")
     else:
+        logging.error(f"Invalid model_name parameter: {model_name}. Exception raised...")
         raise ValueError("Invalid model_name. Supported models: 'resnet50', 'densenet121', 'inception_v3'")
     
     # Freeze all model parameters
     for param in model.parameters():
         param.requires_grad = False
+    logging.debug("All parameters frozen...")
 
     # Replace classifier
     classifier = getattr(model, classifier_name)
     in_features = classifier.in_features
     setattr(model, classifier_name, nn.Linear(in_features, num_classes))
+    logging.debug(f"Classifier replaced from {in_features} to {num_classes} features.")
 
     # Unfreeze classifier layer
     classifier = getattr(model, classifier_name)
     for param in classifier.parameters():
         param.requires_grad = True
+    logging.debug("Classifier layer unfrozen...")
     
     # Unfreeze model specific layers for training
     if training_depth == "last_layer":
         if model_name == "resnet50":
             for param in model.layer4.parameters():
                 param.requires_grad = True
+            logging.debug("Resnet50 layer4 unfrozen for training...")
         elif model_name == "densenet121":
             for param in model.features.denseblock4.parameters():
                 param.requires_grad = True
             for param in model.features.norm5.parameters():
                 param.requires_grad = True
+            logging.debug("Densenet121 denseblock4 and norm5 unfrozen for training...")   
         elif model_name == "inception_v3":
             for param in model.Mixed_7c.parameters():
                 param.requires_grad = True
+            logging.debug("Inception_V3 mixed_7c unfrozen for training...")
 
     elif training_depth == "last_2_layers":
         if model_name == "resnet50":
@@ -193,6 +209,7 @@ def prepare_model(model_name: str, num_classes: int, training_depth: str) -> tor
                 param.requires_grad = True
             for param in model.layer4.parameters():
                 param.requires_grad = True
+            logging.debug("Resnet50 layer4 and layer3 unfrozen for training...")
         elif model_name == "densenet121":
             for param in model.features.denseblock3.parameters():
                 param.requires_grad = True
@@ -202,15 +219,19 @@ def prepare_model(model_name: str, num_classes: int, training_depth: str) -> tor
                 param.requires_grad = True
             for param in model.features.norm5.parameters():
                 param.requires_grad = True
+            logging.debug("DenseNet121 denseblock3, transition3, denseblock4, and norm5 unfrozen for training...")
         elif model_name == "inception_v3":
             for param in model.Mixed_7b.parameters():
                 param.requires_grad = True
             for param in model.Mixed_7c.parameters():
                 param.requires_grad = True
+            logging.debug("Inception_V3 mixed_7b and mixed_7c unfrozen for training...")
     else:
         if training_depth !="classifier_only":
+            logging.error(f"Incoreect parameter passed for model training depth: {training_depth}")
             raise ValueError(f"Incoreect parameter passed for model training depth: {training_depth}")
-
+        
+    logging.debug(f"Model {model_name} prepared for training...")
     return model
 
 # code adapted from https://docs.pytorch.org/tutorials/beginner/transfer_learning_tutorial.html
@@ -236,19 +257,25 @@ def train_model(model_name: str,
     Returns:
         history(dict): Dictionary containing the training and validation losses and accuracies. 
     """
+
+    logging.debug(f"Initialising training for {model_name} with LR={learning_rate}, optimizer={optimizer_name}, depth={training_depth}.")
+
     model = model.to(device)
     criterion = nn.CrossEntropyLoss()
     if optimizer_name == "adam":
-        optimizer = optim.Adam(model.parameters(), lr=config["learning_rate"])
+        optimizer = optim.Adam(model.parameters(), learning_rate)
     elif optimizer_name == "adamw":
-        optimizer = optim.AdamW(model.parameters(), lr=config["learning_rate"])
+        optimizer = optim.AdamW(model.parameters(), learning_rate)
     elif optimizer_name == "sgd":
         optimizer = optim.SGD(model.parameters(), learning_rate, momentum = 0.9) 
     else:
+        logging.error(f"Invalid optimizer parameter:{optimizer_name}.")
         raise ValueError(f"Invalid optimizer selected: {optimizer_name}")
+    logging.debug(f"Optimizer {optimizer_name} initialised with {learning_rate} learning rate.")
 
     scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.5, patience=3)
     early_stop = Earlystopping(patience=5, delta=0.001)
+    logging.debug("Learning rate scheduler and early stopping initialised.")
 
     history = {
         "model_name": [], "LR": [], "training_depth": [], "optimizer": [],
@@ -262,21 +289,21 @@ def train_model(model_name: str,
     best_optimizer_state_dict = None
     best_epoch = 0
 
-
-    print(f"\nStarting training {model_name} on {device}.")
-    print("---------------------------------------------------")
+    logging.debug(f"Starting training {model_name} on {device} for {config["epochs"]} epochs.")
 
     for epoch in range(config["epochs"]):
-        print(f"\nEpoch progress: {epoch+1}/{config["epochs"]}")
-        print("---------------------------------------------------")
+        logging.debug(f"Epoch progress: {epoch+1}/{config["epochs"]}")
 
         train_loss, train_acc = train_epoch(model, train_dataloader,criterion,optimizer,device)
-        print(f"\nTraining loss:  {train_loss:.2f}, Training accuracy: {train_acc:.2f}")
 
         val_acc, val_loss, val_recall, val_precision, val_f1, val_roc_auc, val_specificity = validate_epoch(model, validation_dataloader, criterion, device)
-        print(f"\nValidation loss:  {val_loss:.2f}, Validation accuracy: {val_acc:.2f}, Validation recall: {val_recall:.2f}, Validation precision: {val_precision:.2f}, Validation F1-score: {val_f1:.2f}, Validation ROC-AUC: {val_roc_auc:.2f}, Validation Specificity: {val_specificity:.2f}")
 
+        # Learning rate scheduling
+        old_lr = optimizer.param_groups[0]['lr']
         scheduler.step(1.0 - val_f1)
+        new_lr = optimizer.param_groups[0]['lr']
+        if old_lr != new_lr:
+            logging.info(f"Learning rate changed from {old_lr:.6f} to {new_lr:.6f}")
 
         history["model_name"].append(model_name)
         history["LR"].append(learning_rate)
@@ -298,60 +325,12 @@ def train_model(model_name: str,
             best_model_state_dict = model.state_dict()
             best_optimizer_state_dict = optimizer.state_dict()
             best_epoch = epoch
-            #output_path = os.path.join(config["checkpoints_path"], f"{model_name}_best.pth")
-
-            """ torch.save({
-                "epoch": epoch,
-                "model_state_dict": model.state_dict(),
-                "optimizer_state_dict": optimizer.state_dict(),
-                "val_acc": val_acc,
-                "config": config
-            },output_path)
-            print(f"Model with best accuracy {val_acc:.2f} saved.") """
+            logging.debug(f"New best model saved with F1-score: {val_f1:.4f}.")
         
-        if early_stop(val_acc):
-            print(f"Early stopping forced at epoch {epoch + 1}")
+        if early_stop(1.0 - val_f1):
+            logging.debug(f"Early stopping triggered at epoch {epoch + 1}. Model training stopped, best performing model with F1-score {val_f1:.4f} to be returned")
             return history, best_model_state_dict, best_optimizer_state_dict, best_epoch, best_val_f1
     
+    logging.debug("Model training completed, best performing model with F1-score {val_f1:.4f} to be returned....")
     return history, best_model_state_dict, best_optimizer_state_dict, best_epoch, best_val_f1
 
-""" def main():
-
-    model_names = ["resnet", "densenet", "inception"]
-
-    for model_name in model_names:
-        print(f"\n{"----------------------------------"}")
-        print(f"Training {model_name} starting...")
-        print(f"\n{"----------------------------------"}")
-
-        train_transform, val_transform = apply_transforms(model_name)
-
-        train_mass_df = pd.read_csv("./data/processed/mass_case_description_train_set_mapped.csv")
-        val_mass_df = pd.read_csv("./data/processed/mass_case_description_test_set_mapped.csv")
-
-        train_mass_dataset = CBISDDSMDataset(train_mass_df, transform=train_transform)
-        val_mass_dataset = CBISDDSMDataset(val_mass_df, transform=val_transform)
-
-        train_loader = DataLoader(train_mass_dataset, 
-                              batch_size=config["batch_size"],
-                              shuffle=True,
-                              num_workers=4,
-                              pin_memory=True)
-    
-        val_loader = DataLoader(val_mass_dataset, 
-                              batch_size=config["batch_size"],
-                              shuffle=False,
-                              num_workers=4,
-                              pin_memory=True)
-        
-        model = prepare_model(model_name, config["num_classes"])
-
-        history = train_model(model_name, model, config["device"],
-                              train_loader, val_loader, config)
-        
-        torch.save(history, f"{model_name}_initial_training_history.pth")
-
-if __name__ == "__main__":
-    main()
-    
- """
